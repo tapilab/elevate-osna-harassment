a08cc65d234bc7a373f11f90be1cee2fd387dafb
Aron <aronwc@gmail.com>
2019-07-18 22:09:08 -0500

    need to change hard-coded path

--- osna/cli.py
+++ osna/cli.py
@@ -36,65 +36,67 @@ def web(twitter_credentials, port):
 @click.argument('directory', type=click.Path(exists=True))
 def stats(directory):
     """
-	Read all files in this directory and its subdirectories and print statistics.
-	"""
-	print('reading from %s' % directory)
-	# use glob to iterate all files matching desired pattern (e.g., .json files).
-	# recursively search subdirectories.
-	
-	import pandas as pd
-	df=pd.read_csv('C:\\Users\\郑\\training_data\\data.csv\\data.csv')
-	
-	unique_user=set(df['sender'])
-	n1=len(unique_user)
-	print("The number of unique users is %d"%n1)
-	
-	unique_message=set(df['text'])
-	n2=len(unique_message)
-	print("The number of unique messages is %d"%n2)
-	
-	df1=df[df['hostile']== 0]
-	n3=len(set(df1['sender']))
-	n4=len(set(df1['text']))
-	print("The number of unique users not hostile is %d"%n3)
-	print("The number of unique messages not hostile is %d"%n4)
-	df2=df[df['hostile']== 1]
-	n5=len(set(df2['sender']))
-	n6=len(set(df2['text']))
-	print("The number of unique users hostile is %d"%n5)
-	print("The number of unique messages hostile is %d"%n6)
-	
-	import re
-	def tweet_tokenizer(s):
-	    s = re.sub(r'#(\S+)', r'HASHTAG_\1', s)
-	    s = re.sub(r'@(\S+)', r'MENTION_\1', s)
-	    s = re.sub(r'http\S+', 'THIS_IS_A_URL', s)
-	    return re.sub('\W+', ' ', s.lower()).split()
-	a = []
-	for tweet in df['text']:
-	    a=a+tweet_tokenizer(tweet)
-		
-	n7=len(set(a))
-	n8=len(a)
-	print("The number of unique words is %d"%n7)
-	print("The number of tokens is %d"%n8)
-	
-	from collections import Counter
-	def words(dicts):
-	    counts = Counter() # handy object: dict from object -> int
-	    counts.update(dicts)
-	    return counts
-	counts = words(a)
-	print("The 50 most common words are %s"%counts.most_common(50))
-	
-	b = []
-	for tweet in df1['text']:
-	    b=b+tweet_tokenizer(tweet)
-	c = []
-	for tweet in df2['text']:
-	    c=c+tweet_tokenizer(tweet)
-	print("The 50 most common words not hostile are %s"%words(b).most_common(50))
-	print("The 50 most common words not hostile are %s"%words(c).most_common(50))
+    Read all files in this directory and its subdirectories and print statistics.
+    """
+    print('reading from %s' % directory)
+    # use glob to iterate all files matching desired pattern (e.g., .json files).
+    # recursively search subdirectories.
+    
+    import pandas as pd
+    # use the directory variable (passed into this function), 
+    # rather than hard-coding this path.
+    df=pd.read_csv('C:\\Users\\郑\\training_data\\data.csv\\data.csv')
+    
+    unique_user=set(df['sender'])
+    n1=len(unique_user)
+    print("The number of unique users is %d"%n1)
+    
+    unique_message=set(df['text'])
+    n2=len(unique_message)
+    print("The number of unique messages is %d"%n2)
+    
+    df1=df[df['hostile']== 0]
+    n3=len(set(df1['sender']))
+    n4=len(set(df1['text']))
+    print("The number of unique users not hostile is %d"%n3)
+    print("The number of unique messages not hostile is %d"%n4)
+    df2=df[df['hostile']== 1]
+    n5=len(set(df2['sender']))
+    n6=len(set(df2['text']))
+    print("The number of unique users hostile is %d"%n5)
+    print("The number of unique messages hostile is %d"%n6)
+    
+    import re
+    def tweet_tokenizer(s):
+        s = re.sub(r'#(\S+)', r'HASHTAG_\1', s)
+        s = re.sub(r'@(\S+)', r'MENTION_\1', s)
+        s = re.sub(r'http\S+', 'THIS_IS_A_URL', s)
+        return re.sub('\W+', ' ', s.lower()).split()
+    a = []
+    for tweet in df['text']:
+        a=a+tweet_tokenizer(tweet)
+        
+    n7=len(set(a))
+    n8=len(a)
+    print("The number of unique words is %d"%n7)
+    print("The number of tokens is %d"%n8)
+    
+    from collections import Counter
+    def words(dicts):
+        counts = Counter() # handy object: dict from object -> int
+        counts.update(dicts)
+        return counts
+    counts = words(a)
+    print("The 50 most common words are %s"%counts.most_common(50))
+    
+    b = []
+    for tweet in df1['text']:
+        b=b+tweet_tokenizer(tweet)
+    c = []
+    for tweet in df2['text']:
+        c=c+tweet_tokenizer(tweet)
+    print("The 50 most common words not hostile are %s"%words(b).most_common(50))
+    print("The 50 most common words not hostile are %s"%words(c).most_common(50))
 
 
 if __name__ == "__main__":
