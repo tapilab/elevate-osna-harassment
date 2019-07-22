38772346b578be06d5f45acbc56747ebc60017a3
Riddles <xhl0517@outlook.com>
2019-07-22 13:12:55 -0500

    W2L

--- osna/app/routes.py
+++ osna/app/routes.py
@@ -1,21 +1,28 @@
 from flask import render_template, flash, redirect, session
 from . import app
 from .forms import MyForm
-#from ..mytwitter import Twitter
+from ..mytwitter import Twitter
 #from ..u import get_twitter_data, N_TWEETS
 from .. import credentials_path
 import sys
+import json
+from TwitterAPI import TwitterAPI
 
-#twapi = Twitter(credentials_path)
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+
 	form = MyForm()
 	result = None
 	if form.validate_on_submit():
 		input_field = form.input_field.data
 		flash(input_field)
-		return render_template('myform.html', title='', form=form)
+		t = Twitter(credentials_path)
+		tweetsj = t._get_tweets('screen_name',input_field,limit=200)
+		tweets = [words['full_text'] for words in tweetsj]
+		#Tweets = a list of dict
+		return render_template('myform.html', title='', form=form, tweets=tweets)
 		#return redirect('/index')
 	return render_template('myform.html', title='', form=form)
